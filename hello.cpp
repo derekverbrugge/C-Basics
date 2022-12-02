f8d25d50f89325a9b3d3384ba9351ee0aa3afb00
Derek Verbrugge <52356885+derekverbrugge@users.noreply.github.com>
2022-12-02 14:40:22 -0500

    Create hello.cpp
    Adds hello world.

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+// Hello world is the most primitive example of a program there is. 
+// All it does is print "Hello World" to the console.
+// -Derek Verbrugge
+
+#include <iostream> //including input output stream
+
+using namespace std; //using standard namespace
+
+int main()
+{
+    //prints "Hello World"
+    cout<<"Hello World"; 
+    
+    //All programs in C++ must return something. 'return 0;' ends the program.
+    return 0; 
+}
